03c2c40a9d647b5719b8bb30b522ed000eeb5d90
iamvincentnam <vincentnam22@gmail.com>
2023-05-30 23:58:26 +0100

    began developing a each pages

---
+++ feedback.php
@@ -0,0 +1,20 @@
+<?php include 'header.php'; ?>
+<div class="row">
+
+<div class="card w-75 mb-3 mx-auto col-md-12  text-center my-5">
+<div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <div class="card-footer text-body-secondary">
+    2 days ago
+  </div>
+   
+  </div>
+</div>
+</div>
+<?php include 'footer.php'; ?>
+
+
---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="row">
+            <div class="col-md-12 text-center fw-bold py-3 mt-4 bg-dark text-white-50">
+                <p id="copyright" class="copyright  m-0">Copyright &#169; 2023 Ikokuonline</p>
+        </div>
+        </div>
+           
+    </section>
+</body>
+</html>
